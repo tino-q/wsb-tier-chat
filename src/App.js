5946c7d1e5c07bd12114cd019f1ccfa4f6a4bb26
Martin Queija <Martin.Queija@adeccogroup.com>
2021-01-28 20:55:33 +0100

    bootstrap stonk.chat with cra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import img from './wsb.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
