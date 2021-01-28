96372c4f08b27ce0da786479d9496dfadb0264a2
Martin Queija <Martin.Queija@adeccogroup.com>
2021-01-28 21:21:05 +0100

    add links to social media and remove rotation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import img from './wsb.jpg';
 import './App.css';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  color: black;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Link href="https://discord.gg/gXcqsepVm6">wsb devs discord</Link>
+        <Link href="https://www.reddit.com/r/wallstreetbets/">r/wallstreetbets</Link>
         <img src={img} className="App-logo" alt="logo" />
       </header>
     </div>
